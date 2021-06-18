b5609f0ab0b4dbf9e40168d3bcd335a96878b6b1
Faithful <faithfuladvocate@gmail.com>
2021-06-18 06:42:00 +0100

    new

---
+++ my info/my info.js
@@ -0,0 +1,14 @@
+let myName = "Faithful";
+let gender = "Female";
+let nationality = "Nigerian";
+const dateOfBirth = "14/09";
+let phoneNo = "08089513800";
+let email = "faithfuladvocate@gmail.com";
+let github = "https://github.com/faithfuladvocate/7-days-of-js";
+let profession = "nursing informaticist"
+console.log("My name is", myName);
+console.log("I am a", gender);
+console.log("I am a", nationality);
+console.log("I was born on", dateOfBirth);
+console.log("You can reach me on", phoneNo, ",", email, "and", github);
+console.log("I am an aspiring", profession);
